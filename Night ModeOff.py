580af585440d964b4e0a04c3584bd16c03a300be
roxgib <33942237+roxgib@users.noreply.github.com>
2021-07-29 12:49:11 +1000

    Uploading initial scripts

---
+++ Night ModeOff.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Night mode off. Make sure to set USER to your user account
+
+with open('/Users/USER/Library/Application Support/Anki2/prefs21.db', 'r+b') as f:
+	content = f.read()
+	f.seek(0)
+	f.write(content.replace(b'night_mode\x94\x88u.\n', b'night_mode\x94\x89u.\n'))
